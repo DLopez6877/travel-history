0aaf42deb31d9d079de4a30d27f51d8c51ce6c86
Karina Roush and daniel lopez <karina+daniel@>
2017-05-08 11:37:39 -0700

    add photo option

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 /////////////////////////////////////////////////////////////
 ///////////               BACK_END                ///////////
 /////////////////////////////////////////////////////////////
-function Place(name, date, activities) {
+function Place(name, photo, date, activities) {
   this.placeName = name;
+  this.placePhoto = photo;
   this.placeDate = date;
   this.placeActivities = activities;
 }
@@ -17,11 +18,12 @@ $(function() {
 
     //take user input from form
     var newPlaceName = $("input#name-destination").val();
+    var newPlacePhoto = $("input#photo-destination").val();
     var newPlaceDate = $("input#date-destination").val();
     var newPlaceActivities = $("textarea#activities-destination").val();
 
     // created objects from form input
-    var newPlace = new Place(newPlaceName, newPlaceDate, newPlaceActivities);
+    var newPlace = new Place(newPlaceName, newPlacePhoto, newPlaceDate, newPlaceActivities);
 
     //provide fake link for user to click to show results on right side of screen
     $("ul#list-results").append("<li><span class='user-result'>" + newPlace.placeName + "</span></li>");
@@ -30,6 +32,7 @@ $(function() {
     $(".user-result").last().click(function() {
       $("#rightColResults").show();
       $("#rightColResults h2").text(newPlace.placeName);
+      $(".result-photo").after("<img src='" + newPlace.placePhoto + "'/>")
       $(".result-name").text(newPlace.placeName);
       $(".result-dates").text(newPlace.placeDate);
       $(".result-activities").text(newPlace.placeActivities);
